3e232314221059442391fd55ebbd26c4b7720eda
MonickaMagesh <93465764+monickamagesh@users.noreply.github.com>
2025-01-03 09:02:48 +0530

    Update Navbar.jsx

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -6,18 +6,18 @@ import { useSelector } from "react-redux";
 const Navbar = () => {
   // Navigation items based on user roles
   const therapistNav = [
-    { path: "/therapist/patient-analytic", txt: "Patients Analytic", icon: "analytics" },
-    { path: "/therapist/regimens", txt: "Your Regimens", icon: "Edit" },
-    { path: "/therapist/chat", txt: "Chat", icon: "chat" },
-    { path: "/therapist/notification", txt: "Notification", icon: "notifications" },
+    { path: "/therapist/patient-analytic", txt: "Patients' Analytics", icon: "analytics" },
+    { path: "/therapist/regimens", txt: "Regimens", icon: "Edit" },
+    { path: "/therapist/chat", txt: "Message", icon: "chat" },
+    { path: "/therapist/notification", txt: "Notifications", icon: "notifications" },
     { path: "/therapist/resource-library", txt: "Resource Library", icon: "local_library" },
   ];
   const patientNav = [
-    { path: "/patient/progress", txt: "Your Progress", icon: "offline_bolt" },
-    { path: "/patient/chat", txt: "Chat", icon: "chat" },
-    { path: "/patient/notification", txt: "Notification", icon: "notifications" },
+    { path: "/patient/progress", txt: "Progress Tracker", icon: "offline_bolt" },
+    { path: "/patient/chat", txt: "Message", icon: "chat" },
+    { path: "/patient/notification", txt: "Notifications", icon: "notifications" },
     { path: "/patient/resource-library", txt: "Resource Library", icon: "local_library" },
-    { path: "/patient/analytic", txt: "Your Analytic", icon: "analytics" },
+    { path: "/patient/analytic", txt: "Analytics", icon: "analytics" },
   ];
   const adminNav = [
     { path: "/admin/users", txt: "Manage Users", icon: "group" },
