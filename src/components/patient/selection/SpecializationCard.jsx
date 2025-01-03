4647f550168f75a53bd622f943aa859d877fea34
MonickaMagesh <93465764+monickamagesh@users.noreply.github.com>
2025-01-03 09:45:26 +0530

    Update SpecializationCard.jsx

--- src/components/patient/selection/SpecializationCard.jsx
+++ src/components/patient/selection/SpecializationCard.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import '../../../pages/patient/selection/byspec.css';
+import { Link } from "react-router-dom"
 
 const SpecCard = ({ image, title }) => {
   return (
     <div className="pain-card">
       <img src={image} alt={title} className="pain-image" />
       <h3 className="pain-title">{title}</h3>
-      <button className="view-btn">View Therapists</button>
+      <Link to="top-therapist"><button className="view-btn">View Therapists</button></Link>
     </div>
   );
 };
